22e915d2b7bb7b16475921719ad75e1f5a1d3b0c
ManlioWu <manliowu@ruc.edu.cn>
2025-06-12 15:58:43 +0800

    fix bug (lr is too large)

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -17,7 +17,7 @@ relax_data_path='/path/to/relax_data.pt'
 
 CUDA_VISIBLE_DEVICES='0,1,2' python diffcsp/run.py expname=DAO_G_Stage2 \
     data=pretrain_daog_data data.train_max_epochs=$EPOCHS train.pretrain=True \
-    optim.lr_scheduler_cos.T_0=$EPOCHS optim.optimizer.lr=0.0003  optim.warm_epochs=50\
+    optim.lr_scheduler_cos.T_0=$EPOCHS optim.optimizer.lr=0.0001  optim.warm_epochs=50\
     +load_state_dict_only=True +ckpt_path=$CKPT\
     data.datamodule.datasets.train.save_path=${relax_data_path} \
     model.only_diffusion=True train.pl_trainer.gpus=3
