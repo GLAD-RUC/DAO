ca0904f83e72569220344c7bc55792d80ad25e29
ManlioWu <manliowu@ruc.edu.cn>
2025-06-16 17:13:32 +0800

    update

--- structure_evaluation.sh
+++ structure_evaluation.sh
@@ -7,14 +7,10 @@ MODEL_PATH="/dir/to/finetune_DAO_G.ckpt"
 ### 1 or 20
 NUM_EVALS=1
 LABEL="${NUM_EVALS}_all"
-RANK="False"
-CLOSEST="True"
 echo "data: ${DATA}"
 echo "label: ${LABEL}"
 echo "model: ${MODEL_PATH}"
 echo "num_evals: ${NUM_EVALS}"
-echo "rank_selected : ${RANK}"
-echo "select closest : ${CLOSEST}"
 
 
 #### compute metric: match_rate, rmsd
